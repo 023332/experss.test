da29d42a5572ae7cefab53b939ed203d07387e0f
Karen <nahapetyankaren422@gmail.com>
2025-03-02 14:05:05 +0400

    Minor change

--- routes/comments.js
+++
@@ -1,9 +0,0 @@
-import  {Router} from "express";
-import controller from "../controllers/comments.controller.js";
-
-const router = Router();
-
-router.post("/list",controller.getCommentsList)
-router.post("/create",controller.createComment)
-
-export default router;
