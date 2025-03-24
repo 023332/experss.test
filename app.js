7057f8a3e1c6e8653d9d14ade9981b5a09210159
karen <nahapetyankaren422@gmail.com>
2025-03-25 00:16:40 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ import otherRoutes from './routes/otherRoutes.js';
 
 const app = express();
 
-// Set the view engine to EJS
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
 
-// Routes
+
 app.use('/auth', authRoutes);
 app.use('/other', otherRoutes);
 
-// Home route
+
 app.get('/', (req, res) => {
     res.render('index');
 });
